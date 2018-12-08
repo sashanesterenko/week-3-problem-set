9bcba8473c24d34febc0480d3fca978b88b9aab0
Sasha Nesterenko <nesterenko.sasha@gmail.com>
2018-12-08 23:25:36 +0300

    Две задачи, остальные были в две строки, не стала сохранять.

--- csv_problem.py
+++ csv_problem.py
@@ -5,9 +5,15 @@ dicts = [
             {'name': 'Basil', 'age': 8, 'job': 'Programmer'},
             {'name': 'Edward', 'age': 48, 'job': 'Big boss'}
         ]
-with open('csv_problem.csv', 'w', encoding='utf-8') as f:
-    fields = ['name', 'age', 'job']
-    writer = csv.DictWriter(f, fields, delimiter=';')
-    writer.writeheader()
-    for user in dicts:
-        writer.writerow(user)
+
+def dict_to_csv (dicts):
+    with open('csv_problem.csv', 'w', encoding='utf-8') as f:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for user in dicts:
+            writer.writerow(user)
+        #нужен ли здесь return?
+
+if __name__ == "__main__":
+    dict_to_csv(dicts)
--- file_work.py
+++ file_work.py
@@ -1,13 +1,21 @@
-with open("referat.txt", "r", encoding = "utf-8") as file:
-	cumulative = ""
-	for current in file:
-		cumulative = " ".join([cumulative, current])
-	print(cumulative)	
-cumulative = str(cumulative)
-splited_cumulative = cumulative.split(" ")
-cumulative_exclamation = cumulative.replace(".", "!")
-print(cumulative_exclamation)
-referat2_to_file = "\n".join([str(cumulative), "Length of the cumulative string is {} symbols".format(len(cumulative)), "Number of words in the cumulative string is {}".format(len(splited_cumulative)), str(cumulative_exclamation)])
-with open('referat2.txt', 'w', encoding = 'utf-8') as second_file:
-    second_file.write(referat2_to_file)
 
+file_to_read = "referat.txt"
+file_to_write = "referat2.txt"
+
+def referat_mod(file_to_read, file_to_write):
+    with open(file_to_read, "r", encoding = "utf-8") as file:
+    	# cumulative = ""
+    	# for current in file:
+    	# 	cumulative = " ".join([cumulative, current])
+    	# print(cumulative)	
+        cumulative = file.read()
+    cumulative = str(cumulative)
+    splited_cumulative = cumulative.split(" ")
+    cumulative_exclamation = cumulative.replace(".", "!")
+    referat2_to_file = "\n".join([str(cumulative), "Length of the cumulative string is {} symbols".format(len(cumulative)), "Number of words in the cumulative string is {}".format(len(splited_cumulative)), str(cumulative_exclamation)])
+    with open(file_to_write, 'w', encoding = 'utf-8') as second_file:
+        second_file.write(referat2_to_file)
+
+
+if __name__ == "__main__":
+    referat_mod(file_to_read, file_to_write)
